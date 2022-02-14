f3f92f64b8e07eac64bbafd333e0a85f3ef355d4
aidingh <aidingh@hotmail.com>
2022-02-14 08:50:15 +0100

    CHORE: Added comments to main.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,13 +7,23 @@ import Heroes.Warrior;
 import Item.Item;
 import Weapons.Weapons;
 
+/**
+ * @author Aidin Ghassemloi
+ * Main class for creating heros and items.
+ * Below are some examples on how instances are created and usage.
+ */
+
 public class Main {
 
     public static void main(String[] args) throws InvalidArmourException, InvalidWeaponException {
 
+        //Creates an instance of a warrior hero class.
         Warrior warrior = new Warrior("Muradin Bronzebeard");
+
+        //Print hero attributes. Such as dps, base attributes and more.
         System.out.println(warrior.toString());
 
+        //Creates an instance of weapon. And set the attributes. There is also an option to create an object by the contructor aswell.
         Weapons axe = new Weapons();
         axe.setItemName("Common Axe");
         axe.setItemLevel(1);
@@ -22,9 +32,11 @@ public class Main {
         axe.setDmg(7);
         axe.setAttackSpeed(1.1);
 
+        //Equip the Weapons instance and print the new attributes. As the dps will change depending on weapon.
         boolean didEquipWeapon = warrior.equipWeapon(axe);
         System.out.println(warrior.toString());
 
+        //Creates an instance of armour. And set the attributes. There is also an option to create an object by the contructor aswell.
         Armours plateBodyArmour = new Armours();
         plateBodyArmour.setItemName("Common Plate Body Armour");
         plateBodyArmour.setItemLevel(1);
@@ -32,11 +44,8 @@ public class Main {
         plateBodyArmour.setArmourType(Armours.ArmourItems.PLATE);
         plateBodyArmour.setPrimaryAttributes(new PrimaryAttributes(1,0,0));
 
-
+        //Equip the Armours instance and print the new attributes. As the dps will change depending on weapon and armour..
         boolean didEquipArmour = warrior.equipArmour(plateBodyArmour);
         System.out.println(warrior.toString());
-
     }
-
-
 }
